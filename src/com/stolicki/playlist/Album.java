68096c869dc5fda145e73ed042fe6e728cfaf75d
100licki <karol.stolicki@gmail.com>
2021-07-17 01:30:00 +0200

    Playlist update

--- src/com/stolicki/playlist/Album.java
+++ src/com/stolicki/playlist/Album.java
@@ -6,62 +6,64 @@ import java.util.LinkedList;
 public class Album {
     private String name;
     private String artist;
-    private ArrayList<Song> songs;
+    private SongList songs;
 
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        this.songs = new ArrayList<Song>();
-    }
-
-    public static void main(String[] args) {
-        ArrayList<Album> albums = new ArrayList<Album>();
-        Album album = new Album("nazwa1", "artysta1");
-        album.addSong("tytul1", 1.30);
-        album.addSong("tytul2", 2.30);
-        album.addSong("tytul3", 3.30);
-        album.addSong("tytul4", 4.30);
-
-        LinkedList<Song> playlist = new LinkedList<Song>();
-        albums.get(0).addToPlayList("tytul1", playlist);
-
-
-    }
-
-    private Song findSong(String songTitle) {
-        if (!songs.isEmpty()) {
-            for (int i = 0; i <= this.songs.size() - 1; i++) {
-                if (songs.get(i).getTitle().equals(songTitle)) {
-                    return songs.get(i);
-                }
-            }
-        }
-        return null;
+        this.songs = new SongList();
     }
 
     public boolean addSong(String title, double duration) {
-        if (findSong(title) == null) {
-            songs.add(new Song(title, duration));
-            return true;
-        }
-        return false;
+        return this.songs.add(new Song(title, duration));
     }
 
     public boolean addToPlayList(int trackNumberInAlbum, LinkedList<Song> playlist) {
-        if (trackNumberInAlbum > 0) {
-            if (findSong(songs.get(trackNumberInAlbum - 1).getTitle()) != null) {
-                playlist.add(findSong(songs.get(trackNumberInAlbum - 1).getTitle()));
-                return true;
-            }
+        if (this.songs.findSong(trackNumberInAlbum) != null) {
+            playlist.add(this.songs.findSong(trackNumberInAlbum));
+            return true;
         }
         return false;
     }
 
     public boolean addToPlayList(String songTitle, LinkedList<Song> playlist) {
-        if (findSong(songTitle) != null) {
-            playlist.add(findSong(songTitle));
+        if (this.songs.findSong(songTitle) != null) {
+            playlist.add(this.songs.findSong(songTitle));
             return true;
         }
         return false;
+    }
+
+    public static class SongList {
+        private ArrayList<Song> songs;
+
+        private SongList() {
+            this.songs = new ArrayList<Song>();
+        }
+
+        private boolean add(Song song) {
+            if (!songs.contains(song)) {
+                this.songs.add(song);
+                return true;
+            }
+            return false;
+        }
+
+        private Song findSong(String songTitle) {
+            for (int i = 0; i <= this.songs.size() - 1; i++) {
+                if (this.songs.get(i).getTitle().equals(songTitle)) {
+                    return this.songs.get(i);
+                }
+            }
+            return null;
+        }
+
+        private Song findSong(int trackNumber) {
+            int index = trackNumber - 1;
+            if ((index > 0) && (index < songs.size())) {
+                return songs.get(index);
+            }
+            return null;
+        }
     }
 }
