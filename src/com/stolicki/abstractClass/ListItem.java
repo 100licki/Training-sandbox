828b42de60e3fe94ecd72acc2ace4c6b61a9e78d
100licki <karol.stolicki@gmail.com>
2021-08-08 02:12:44 +0200

    Abstract classes, Binary Search Tree, Linked List

---
+++ src/com/stolicki/abstractClass/ListItem.java
@@ -0,0 +1,29 @@
+package com.stolicki.abstractClass;
+
+public abstract class ListItem {
+    protected ListItem rightLink;
+    protected ListItem leftLink;
+    protected Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
+
+    abstract ListItem next();
+
+    abstract ListItem setNext(ListItem listItem);
+
+    abstract ListItem previous();
+
+    abstract ListItem setPrevious(ListItem listItem);
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    abstract int compareTo(ListItem listItem);
+}
---
+++ src/com/stolicki/abstractClass/SearchTree.java
@@ -0,0 +1,109 @@
+package com.stolicki.abstractClass;
+
+public class SearchTree implements NodeList {
+    private ListItem root;
+
+    public SearchTree(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem listItem) {
+        if (this.root == null) {
+            this.root = listItem;
+            return true;
+        }
+        ListItem currentRoot = this.root;
+        while (currentRoot != null) {
+            int comparison = currentRoot.compareTo(listItem);
+            if (comparison < 0) {
+                if (currentRoot.next() == null) {
+                    currentRoot.setNext(listItem);
+                    return true;
+                }
+                currentRoot = currentRoot.next();
+            } else if (comparison > 0) {
+                if (currentRoot.previous() == null) {
+                    currentRoot.setPrevious(listItem);
+                    return true;
+                }
+                currentRoot = currentRoot.previous();
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        if (item != null) {
+            System.out.println("Deleting item " + item.getValue());
+        }
+        ListItem currentItem = this.root;
+        ListItem parentItem = currentItem;
+
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(item));
+            if (comparison < 0) {
+                parentItem = currentItem;
+                currentItem = currentItem.next();
+            } else if (comparison > 0) {
+                parentItem = currentItem;
+                currentItem = currentItem.previous();
+            } else {
+                performRemoval(currentItem, parentItem);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void performRemoval(ListItem item, ListItem parent) {
+        if (item.next() == null) {
+            if (parent.next() == item) {
+                parent.setNext(item.previous());
+            } else if (parent.previous() == item) {
+                parent.setPrevious(item.previous());
+            } else {
+                this.root = item.previous();
+            }
+        } else if (item.previous() == null) {
+            if (parent.next() == item) {
+                parent.setNext(item.next());
+            } else if (parent.previous() == item) {
+                parent.setPrevious(item.next());
+            } else {
+                this.root = item.next();
+            }
+        } else {
+
+            ListItem current = item.next();
+            ListItem leftmostParent = item;
+            while (current.previous() != null) {
+                leftmostParent = current;
+                current = current.previous();
+            }
+            item.setValue(current.getValue());
+            if (leftmostParent == item) {
+                item.setNext(current.next());
+            } else {
+                leftmostParent.setPrevious(current.next());
+            }
+        }
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+        if (root != null) {
+            traverse((root.previous()));
+            System.out.println(root.getValue());
+            traverse(root.next());
+        }
+    }
+}
